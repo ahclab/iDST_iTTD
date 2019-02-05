7a50ecc16e3765962a71ce37345db18a7c7e74ad
Coman_Catalin Andrei <andrei-cc@ahcpascal05.naist.jp>
2019-02-05 13:09:41 +0900

    refactored model

--- idst_util/dstc2.py
+++ idst_util/dstc2.py
@@ -1,5 +1,5 @@
 from idst_util import trivial
-from tqdm import tqdm
+from tqdm import tqdm_notebook
 import os
 import copy
 import wget
@@ -71,15 +71,19 @@ def check(path = "."):
             os.remove(os.path.join(path, file_name))
     logging.info("Done!")
 
-def extract_raw_features(data_directory, flist, ontology, data_augmentation = False):
+    
+def extract_raw_features(data_directory, flist, ontology, data_augmentation = False, tokenize_slot_element = False, weight = 0.0):
     X = []
     Y = []
-    for dialog_path in tqdm(flist):
+    for dialog_path in tqdm_notebook(flist, total = len(flist)):
         dialog_directory = os.path.join(data_directory, dialog_path)
+        
         log_json_path = os.path.join(dialog_directory, "log.json")
         label_json_path = os.path.join(dialog_directory, "label.json")
+        
         log_json = {}
         label_json = {}
+        
         with open(log_json_path, "r") as log_json_file, open(label_json_path, "r") as label_json_file:
             log_json = json.load(log_json_file)
             label_json = json.load(label_json_file)
@@ -95,6 +99,7 @@ def extract_raw_features(data_directory, flist, ontology, data_augmentation = Fa
         X_dialog_augmented = {}
         X_dialog_augmented["session-id"] = log_json["session-id"]
         X_dialog_augmented["turns"] = []
+        
         Y_dialog_augmented = {}
         Y_dialog_augmented["session-id"] = log_json["session-id"]
         Y_dialog_augmented["turns"] = []
@@ -105,23 +110,30 @@ def extract_raw_features(data_directory, flist, ontology, data_augmentation = Fa
             X_dialog_turn = {}
             X_dialog_turn["system"] = []
             X_dialog_turn["user"] = []
+            
             # X_turn: dialog-act
             for dialog_act in log_turn["output"]["dialog-acts"]:
                 act = dialog_act["act"]
                 slots = dialog_act["slots"]
-                X_dialog_turn["system"].append((act, 0))
+                X_dialog_turn["system"].append((act, weight))
                 if len(slots) != 0:
                     for slot in slots:
                         for slot_element in slot:
-                            slot_element = str(slot_element) # this is for the count in test
-                            for slot_value in nltk.word_tokenize(slot_element):
-                                X_dialog_turn["system"].append((slot_value, 0))
+                            slot_element = str(slot_element) # this is for the count in test. Check handbook for detail
+                            if tokenize_slot_element:
+                                for slot_value in nltk.word_tokenize(slot_element):
+                                    X_dialog_turn["system"].append((slot_value, weight))
+                            else:
+                                X_dialog_turn["system"].append((slot_element, weight))
+            
+            # LecTrack: 3.2.1 Including Transcriptions in Training Data
             # X_turn: asr-hyps
             asr_hyps = log_turn["input"]["live"]["asr-hyps"]
             asr_hyp = asr_hyps[0]["asr-hyp"]
-            asr_score = asr_hyps[0]["score"]
+            asr_score = asr_hyps[0]["score"] # take ASR 1-best
             for asr_token in nltk.word_tokenize(asr_hyp):
                 X_dialog_turn["user"].append((asr_token, asr_score))
+                
             X_dialog["turns"].append(X_dialog_turn)
             
             # Y_turn
@@ -133,14 +145,17 @@ def extract_raw_features(data_directory, flist, ontology, data_augmentation = Fa
             Y_dialog_turn["goal"]["pricerange"] = None
             Y_dialog_turn["goal"]["name"] = None
             Y_dialog_turn["goal"]["area"] = None
+            
             # Y_turn: method-label
             Y_dialog_turn["method"] = label_turn["method-label"]
+            
             # Y_turn: requested-slots
             Y_dialog_turn["requested"] = label_turn["requested-slots"]
             Y_dialog_turn["goal"]["food"] = label_turn["goal-labels"].get("food", None)
             Y_dialog_turn["goal"]["pricerange"] = label_turn["goal-labels"].get("pricerange", None)
             Y_dialog_turn["goal"]["name"] = label_turn["goal-labels"].get("name", None)
             Y_dialog_turn["goal"]["area"] = label_turn["goal-labels"].get("area", None)
+            
             Y_dialog["turns"].append(Y_dialog_turn)
             
             assert len(X_dialog["turns"]) == len(Y_dialog["turns"])
@@ -149,10 +164,10 @@ def extract_raw_features(data_directory, flist, ontology, data_augmentation = Fa
             if data_augmentation:
                 X_dialog_turn_augmented = copy.deepcopy(X_dialog_turn)
                 Y_dialog_turn_augmented = copy.deepcopy(Y_dialog_turn)
-                X_dialog_turn_augmented["user"] = []
+                #X_dialog_turn_augmented["user"] = [] # Uncomment this to erase user ASR hypothesis so far and keep only the true transcription
                 transcription = label_turn["transcription"]
                 for transcription_token in nltk.word_tokenize(transcription):
-                    X_dialog_turn_augmented["user"].append((transcription_token, 0))
+                    X_dialog_turn_augmented["user"].append((transcription_token, weight))
                 X_dialog_augmented["turns"].append(X_dialog_turn_augmented)
                 Y_dialog_augmented["turns"].append(Y_dialog_turn_augmented)
             
@@ -192,9 +207,9 @@ def retrieve_raw_datasets(train_data_augmentation = False):
     with open(train_flist_path, "r") as train_flist_file, open(dev_flist_path, "r") as dev_flist_file:
         train_flist = train_flist_file.read().split()
         dev_flist = dev_flist_file.read().split()
-    assert len(train_flist) == 1612 #according to handbook
+    assert len(train_flist) == 1612 # according to handbook
     logging.info("Asserted 1612 dialogs for dstc2_train.flist")
-    assert len(dev_flist) == 506 #according to handbook
+    assert len(dev_flist) == 506 # according to handbook
     logging.info("Asserted 506 dialogs for dstc2_dev.flist")
     with open(dstc2_ontology_path, "r") as dstc2_ontology_file:
         dstc2_ontology = json.load(dstc2_ontology_file)
@@ -209,8 +224,9 @@ def retrieve_raw_datasets(train_data_augmentation = False):
     # DEV
     logging.info("Extracting raw dev features")
     X_dev, Y_dev = extract_raw_features(data_directory = traindev_data_directory,
+                                        flist = dev_flist,
                                         ontology = dstc2_ontology,
-                                        flist = dev_flist)
+                                        data_augmentation = False) # LecTrack: no augmentation should be done on dev
     
     # TEST
     logging.info("Reading dstc2_test.flist")
@@ -221,11 +237,14 @@ def retrieve_raw_datasets(train_data_augmentation = False):
     test_flist = []
     with open(test_flist_path, "r") as test_flist_file:
         test_flist = test_flist_file.read().split()
-    assert len(test_flist) == 1117 #according to handbook
+    assert len(test_flist) == 1117 # according to handbook
     logging.info("Asserted 1117 dialogs for dstc2_test.flist")
     logging.info("Extracting raw test features")
     X_test, Y_test = extract_raw_features(data_directory = test_data_directory,
+                                          flist = test_flist,
                                           ontology = dstc2_ontology,
-                                          flist = test_flist)
- 
+                                          data_augmentation = False) # LecTrack: no augmentation should be done on test
+    
+    logging.info("Done!")
+    
     return np.array(X_train), np.array(Y_train), np.array(X_dev), np.array(Y_dev), np.array(X_test), np.array(Y_test), dstc2_ontology
